e99081c1e18c4d8d522ecc2204efd27760c74953
이도훈 <33706689+leedohun@users.noreply.github.com>
2021-09-22 03:36:52 +0900

    가장 긴 증가하는 부분 수열 2

---
+++ LIS/12015.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+/*#include <iostream>
+#include <string>
+#include <queue>
+#include <set>*/
+
+#define rep(i, n) for(int i = 0; i < (n); i++)
+
+using namespace std;
+
+typedef long long ll;
+
+const ll _INF = 1e18;
+const ll INF = 1e9;
+
+struct P {
+    int x; int y;
+};
+
+struct compare {
+    bool operator()(P& a, P& b) {
+        if (a.x != b.x)
+            return a.x < b.x;
+        return
+            a.y < b.y;
+    }
+};
+priority_queue <P, vector<P>, compare> pq;
+
+bool cmp(const P& a, const P& b) {
+    if (a.x < b.x) return true;
+    else if (a.x == b.x) {
+        if (a.y < b.y) return true;
+    }
+    return false;
+}
+
+vector <int> lis;
+vector <int> v(1000005);
+
+int binarySerach(int left, int right, int target) {
+    while (left < right) {
+        int mid = (left + right) / 2;
+
+        if (lis[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+    return right;
+}
+
+void solved() {
+    int N; cin >> N;
+    rep(i, N) cin >> v[i];
+
+    lis.push_back(v[0]);
+    int i = 1;
+
+    while (i < N) {
+        if (lis[lis.size() - 1] < v[i]) {
+            lis.push_back(v[i]);
+        }
+        else {
+            int idx = binarySerach(0, lis.size(), v[i]);
+            lis[idx] = v[i];
+        }
+        i += 1;
+    }
+
+    cout << lis.size();
+}
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    solved();
+    return 0;
+}
