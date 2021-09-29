a7e3ae583f10b3088767a8472d097f126cf963d9
이도훈 <33706689+leedohun@users.noreply.github.com>
2021-09-30 00:41:19 +0900

    평행 우주

---
+++ Greedy/17451.cpp
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+/*#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <map>*/
+
+#define rep(i, n) for(int i = 0; i < (n); i++)
+
+using namespace std;
+
+typedef long long ll;
+
+const ll _INF = 1e18;
+const ll INF = 1e9;
+const int mod = 1000000007;
+const int MAX = 1000005;
+
+struct P {
+    ll x; ll y;
+};
+
+struct compare {
+    bool operator()(P& a, P& b) {
+        if (a.x != b.x)
+            return a.x < b.x;
+        return
+            a.y < b.y;
+    }
+};
+priority_queue <P, vector<P>, compare> pq;
+
+bool cmp(const P& a, const P& b) {
+    if (a.x < b.x) return true;
+    else if (a.x == b.x) {
+        if (a.y < b.y) return true;
+    }
+    return false;
+}
+
+void solved() {
+    int N; cin >> N;
+    vector <ll> v(N);
+    rep(i, N) cin >> v[i];
+
+    ll ans = v[N - 1];
+    for (int i = N - 2; 0 <= i; i--) {
+        if (ans <= v[i]) ans = v[i];
+        else {
+            if (ans % v[i] == 0) continue;
+            else {
+                ans = v[i] * (ans / v[i] + 1);
+            }
+        }
+    }
+    cout << ans;
+}
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    solved();
+    return 0;
+}
