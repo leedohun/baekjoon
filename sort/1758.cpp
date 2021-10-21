2fda0a4d5988e7c4236a157faa3d1036cdb98f2b
이도훈 <33706689+leedohun@users.noreply.github.com>
2021-10-21 21:09:20 +0900

    알바생 강호

---
+++ sort/1758.cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <bitset>
+#include <stack>
+#include <cmath>
+
+#define rep(i, n) for(int i = 0; i < (n); i++)
+
+using namespace std;
+
+typedef long long ll;
+
+const ll _INF = 1e18;
+const ll INF = 1e9;
+const int mod = 1000000007;
+const int MAX = 1000005;
+
+struct P {
+    ll x; ll y; ll cnt;
+};
+
+struct compare {
+    bool operator()(P& a, P& b) {
+        if (a.x != b.x)
+            return a.x > b.x;
+        return
+            a.y > b.y;
+    }
+};
+//priority_queue <P, vector<P>, compare> pq;
+
+bool cmp(const P& a, const P& b) {
+    if (a.x < b.x) return true;
+    else if (a.x == b.x) {
+        if (a.y < b.y) return true;
+    }
+    return false;
+}
+
+int gcd(int a, int b) {
+    while (b != 0) { int r = a % b; a = b; b = r; }
+    return a;
+}
+
+void solved() {
+    int N; cin >> N;
+    vector <int> v(N);
+    rep(i, N) cin >> v[i];
+
+    sort(v.begin(), v.end());
+    ll ans = 0;
+    for (int i = N - 1, j = 0; 0 <= i; i--, j++) {
+        if (v[i] - j > 0) {
+            ans += v[i] - j;
+        }
+        else
+            break;
+    }
+    cout << ans;
+}
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    solved();
+    return 0;
+}
